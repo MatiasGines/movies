55e138be6e7157909490713a7d7f52489bd72c23
MatiasGines <matiasgines47@gmail.com>
2022-10-31 21:24:49 -0300

    mejoramos la perfomance
    mejoramos la perfomance en las busquedas

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinct, filter, fromEvent, map, Observable, Subscription, switchMap, tap } from 'rxjs';
+import { Movie } from 'src/app/interfaces/movies';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -6,18 +8,36 @@ import { MovieService } from 'src/app/services/movie.service';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
-  movies:any[]= [];
+export class MoviesComponent implements OnInit{
+  movies:Movie[]= [];
+
+  @ViewChild('movieSearchInput', { static:true }) movieSearchInput!: ElementRef; 
+
+  movies$!: Observable<Movie[]>
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.movies$ = fromEvent<Event>(this.movieSearchInput.nativeElement, 'keyup').pipe(
+      map((event: Event)=>{
+        const searchTerm = (event.target as HTMLInputElement).value;
+        return searchTerm;
+      }),
+      filter((searchTerm: string)=> searchTerm.length>3),
+      debounceTime(500),
+      distinct(),
+      switchMap((searchTerm: string)=>this.movieService.getMovies(searchTerm)),
+      )
   }
 
-  getMovies(searchTerm: string){
+
+  /*getMovies(searchTerm: string){
     this.movieService.getMovies(searchTerm).subscribe(movies=>{
+      console.log(movies);
       this.movies = movies !== undefined ? movies : [];
     })
-  }
+  }*/
 
 }
+
+
